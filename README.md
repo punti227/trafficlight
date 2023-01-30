448e313d6e9d8df152e09e65525ab9180c59b57f
Puntigam Simon <puntig190080@sr.htlweiz.at>
2023-01-30 16:47:12 +0100

    README added and swap throught colors

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# trafficlight
+# Trafficlight
+
+## One LED Blinking
+One LED blinking is in the first commit the programm
+
+It turns the output on Port4 ON then waits 2 seconds an turns it OFF.
+
+Turning the LED on an off for 2 seconds. It repeats itself until cut of power.
+
+## Trafficlight
+The trafficlight is not fully functioning its a run throught the LEDS
+
+It turn the output on Port4 ON then waits 2 seconds an turns it back OFF. In 100 ms it turns output on PORT2 ON then waits 2 seconds an turns it back OFF. In 100 ms it turns output on PORT3 ON then waits 2 seconds an turns it back OFF.
+
+It turn the first LED on for 2 seconds and then the second LED on for 2 seconds and then the third LED on 2 seconds. It repeats itself until cut of power.
--- main.c
+++ main.c
@@ -1,15 +1,25 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-void main(){
 
-    DDRB = 0b00010000;
+int main(){
+
+    DDRB = 0b00011100; // Variante 1
 
      while (1) {
-        PORTB = 0b00010000; //Port 4 high   LED ON
-        _delay_ms(2000);    //Wait 2 sec
-        PORTB = 0b00000000; // Port 4 Low   LED OFF
-        _delay_ms(2000);    // wait 2 sec
-    }
 
+        PORTB = 0b00010000; //Port 4 high   red LED ON
+        _delay_ms(2000);    //Wait 2 sec
+        PORTB = 0b00000000; // Port 4 Low   red LED OFF
+        _delay_ms(100);    // wait 100 ms
+        PORTB = 0b00000100; //Port 2 high   yellow LED ON
+        _delay_ms(2000);    //Wait 2 sec
+        PORTB = 0b00000100; // Port 2 Low   yellow LED OFF
+        _delay_ms(100);    // wait 100 ms
+        PORTB = 0b00001000; //Port 3 high   green LED ON
+        _delay_ms(2000);    //Wait 2 sec
+        PORTB = 0b00000000; // Port 3 Low   green LED OFF
+        _delay_ms(100);    // wait 100 ms
+    }
+    return(1);
 }
